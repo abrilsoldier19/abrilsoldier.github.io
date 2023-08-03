37657a847df864a3086d91ef03f11d66344a3ab7
abrilsoldier19 <76828591+abrilsoldier19@users.noreply.github.com>
2023-08-03 13:46:07 -0600

    Add files via upload

---
+++ 2023_01_07_072042_create_carreras_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carreras', function (Blueprint $table) {
+            $table->id('IdCarreras');
+            $table->text('NombreCarrera');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carreras');
+    }
+}
